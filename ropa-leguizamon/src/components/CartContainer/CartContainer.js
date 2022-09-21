fd126f810d78b20319f01a0db865d152ea1df32f
Santiago Leguizamon <santiagoleguizamon007@gmail.com>
2022-09-21 12:37:18 -0300

    Rendering

--- ropa-leguizamon/src/components/CartContainer/CartContainer.js
+++ ropa-leguizamon/src/components/CartContainer/CartContainer.js
@@ -1,8 +1,15 @@
-import React,{useContext} from 'React'
+import React,{useContext, useState} from 'React'
 import { CartContext } from '../../context/CartContext'
+import ItemCount from '../ItemCount/ItemCount';
 
 export const CartContainer = () => {
-    const {productCartList,removeItem} = useContext(CartContext)
+    const {productCartList,removeItem} = useContext(CartContext);
+    const {prods, setProds} = useState(ItemDetailContainer);
+    const noItems = () => {
+    const [noHay, setNoHay] = useState(true);
+    const total = () => {
+        {item} + {quantity} == total 
+    }
   return (
     <div>
         CartContainer
@@ -10,13 +17,16 @@ export const CartContainer = () => {
             {productCartList.map(item=>(
                 <>
                 <p>{item.item} - {item.quantity}</p>
-                <button onClick={()=>removeItem(item.id)}>eliminar</button>
+                <button onClick={()=>removeItem(item.id)}>eliminar</button> 
+                {noHay? <h2>No hay productos seleccionados</h2> : productCartList}
+                <button onClick={()=>setProds(ItemDetailContainer)}>Segui comprando!</button>
+                <p>{total}</p>
                 </>
-            
             ))}
         </div>
     </div>
   )
-}
+}}
+
 
 export default CartContainer
